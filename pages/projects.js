cc45362e3813eba1386cc2c3185df0cbd7efca44
Nipuni Muthucumarana <niunimuthucumarana@gmail.com>
2021-07-04 15:38:02 +0530

    Accessed the back-end data

--- pages/projects.js
+++ pages/projects.js
@@ -1,107 +1,134 @@
-import React from 'react'
+import React, { Component } from 'react'
+import fetch from 'isomorphic-fetch'
+import Image from 'next/image'
 
-export default function projects() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-9">
-                    <div className="section-title">
-                        <span className="caption d-block small">Categories</span>
-                        <h2>Politics</h2>
-                    </div>
-                    <div className="post-entry-2 d-flex">
-                        {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_4.jpg')"></div> */}
-                        <img src="images/img_h_4.jpg" alt="Image" className="thumbnail order-md-2" />
-                        <div className="contents order-md-1 pl-0">
-                            <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                            <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <div className="row">
-                                <div className="col-12">
-                                    <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="post-entry-2 d-flex">
-                        {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_3.jpg')"></div> */}
-                        <img src="images/img_h_3.jpg" alt="Image" className="thumbnail order-md-2" />
-                        <div className="contents order-md-1 pl-0">
-                            <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                            <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <div className="row">
-                                <div className="col-12">
-                                    <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+export default class Projects extends Component {
+    constructor(){
+        super();
+        this.state = {
+            projects: []
+        }
+    }
 
-                    <div className="post-entry-2 d-flex">
-                        {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_1.jpg')"></div> */}
-                        <img src="images/img_h_1.jpg" alt="Image" className="thumbnail order-md-2" />
-                        <div className="contents order-md-1 pl-0">               
-                            <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                            <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <div className="row">
-                                <div className="col-12">
-                                    <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    componentWillMount(){
+        fetch('http://localhost:1337/projects').then((response)=>{
+            if(response.status >= 400){
+                throw new Error("Bad Response From Server");
+            }
+            return response.json();
+        }).then((projects) => {
+            this.setState({projects: projects});
+            console.log(projects)
+        });
+    }
 
-                    <div className="post-entry-2 d-flex">
-                        {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_4.jpg')"></div> */}
-                        <img src="images/img_h_4.jpg" alt="Image" className="thumbnail order-md-2" />
-                        <div className="contents order-md-1 pl-0">
-                            <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                            <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <div className="row">
-                                <div className="col-12">
-                                    <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+    render() {
+        return (           
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="section-title">
+                            <span className="caption d-block small">Categories</span>
+                            <h2>Politics</h2>
+                        </div>
+                        {
+                            this.state.projects.map(({id, ProjectName, Abstract, ProjectImage }) => (
+                                <div key={id} className="post-entry-2 d-flex">
+                                    {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_4.jpg')"></div> */}
+                                    <img  width="100%" height="100%"  src={`http://localhost:1337${ProjectImage.url}`} alt="Image" className="thumbnail order-md-2" />
+                                    <div className="contents order-md-1 pl-0">
+                                        <h2><a href="blog-single.html">{ProjectName}</a></h2>
+                                        <p className="mb-3">{Abstract}</p>
+                                        <div className="row">
+                                            <div className="col-12">
+                                                <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                        {/* <div className="post-entry-2 d-flex">
+                            
+                            <img src="images/img_h_3.jpg" alt="Image" className="thumbnail order-md-2" />
+                            <div className="contents order-md-1 pl-0">
+                                <h2><a href="blog-single.html">{ProjectName}</a></h2>
+                                <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="post-entry-2 d-flex">
-                        {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_3.jpg')"></div> */}
-                        <img src="images/img_h_3.jpg" alt="Image" className="thumbnail order-md-2" />
-                        <div className="contents order-md-1 pl-0">
-                            <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                            <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <div className="row">
-                                <div className="col-12">
-                                    <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+                        <div className="post-entry-2 d-flex">
+                            {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_1.jpg')"></div> 
+                            <img src="images/img_h_1.jpg" alt="Image" className="thumbnail order-md-2" />
+                            <div className="contents order-md-1 pl-0">               
+                                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
+                                <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="post-entry-2 d-flex">
-                        {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_1.jpg')"></div> */}
-                        <img src="images/img_h_1.jpg" alt="Image" className="thumbnail order-md-2" />
-                        <div className="contents order-md-1 pl-0">
-                            <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                            <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <div className="row">
-                                <div className="col-12">
-                                    <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+
+                        <div className="post-entry-2 d-flex">
+                            {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_4.jpg')"></div> 
+                            <img src="images/img_h_4.jpg" alt="Image" className="thumbnail order-md-2" />
+                            <div className="contents order-md-1 pl-0">
+                                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
+                                <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <ul className="custom-pagination list-unstyled">
-                            <li><a href="#">1</a></li>
-                            <li className="active">2</li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">4</a></li>
-                            </ul>
+
+                        <div className="post-entry-2 d-flex">
+                            {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_3.jpg')"></div> 
+                            <img src="images/img_h_3.jpg" alt="Image" className="thumbnail order-md-2" />
+                            <div className="contents order-md-1 pl-0">
+                                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
+                                <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="post-entry-2 d-flex">
+                            {/* <div className="thumbnail order-md-2" style="background-image: url('images/img_h_1.jpg')"></div> 
+                            <img src="images/img_h_1.jpg" alt="Image" className="thumbnail order-md-2" />
+                            <div className="contents order-md-1 pl-0">
+                                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
+                                <p className="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div> */}
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <ul className="custom-pagination list-unstyled">
+                                <li><a href="#">1</a></li>
+                                <li className="active">2</li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">4</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
