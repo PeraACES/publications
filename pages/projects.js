7a446ceb0f20bb996563832cf338670cdef1fc5a
Dulanjali <dulapreethi@gmail.com>
2021-07-17 16:47:29 +0530

    images displaying

--- pages/projects.js
+++ pages/projects.js
@@ -3,7 +3,8 @@ import fetch from 'isomorphic-fetch'
 import Link from 'next/link'
 
 const Projects = ({projects, error}) => {
-
+    //console.log(projects);
+    //console.log(projects.ProjectName);
     if (error) {
         return <div>An error occured: {error.message}</div>;
     }
@@ -16,33 +17,37 @@ const Projects = ({projects, error}) => {
                         {/* <span className="caption d-block small">Categories</span>
                         <h2>Projects</h2> */}
                     </div>
+                    <br />
                     {
-                        projects.map((projects) => (
-                            <div key={projects.id} className="post-entry-2 d-flex">
-                                
-                                <img  width="100%" height="100%"  src={`https://aces-admin.herokuapp.com/projects${projects.ProjectImage}`} alt="Image" className="thumbnail order-md-2" />  
-                                <div className="contents order-md-1 pl-0">
-                                    <h2><a href="blog-single.html">{projects.ProjectName}</a></h2>
-                                    <p className="mb-3">{projects.Abstract}</p>
-                                    <div className="row">
-                                        <div className="col-12">
-                                        <Link href={'/projects/' + projects.id}><input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" /></Link>
+                        projects.map((projects) => {
+                            //console.log(projects.ProjectImage1.url);
+
+                            return(
+                                <div key={projects.id} className="post-entry-2 d-flex">
+                                    {/* <img  width="100%" height="100%"  src={projects.ProjectImage1.url} alt="Image" className="thumbnail order-md-2" /> */}
+                                    <div className="contents order-md-1 pl-0">                                        
+                                        <h2><a href="blog-single.html">{projects.ProjectName}</a></h2>
+                                        <p className="mb-3">{projects.Abstract}</p>
+                                        <div className="row">
+                                            <div className="col-12">
+                                            <Link href={'/projects/' + projects.id}><input type="submit" value="View More" className="btn-2 btn-primary py-2 px-8" /></Link>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
+                            );
+                        })
                     }
-                    <div className="row">
+                    {/* <div className="row">
                         <div className="col-lg-6">
                             <ul className="custom-pagination list-unstyled">
-                            <li><a href="#">1</a></li>
-                            <li className="active">2</li>
+                            <li className="active">1</li>
+                            <li ><a href="#">2</a></li>
                             <li><a href="#">3</a></li>
                             <li><a href="#">4</a></li>
                             </ul>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export async function getStaticPaths() {
     const res = await fetch('https://aces-admin.herokuapp.com/projects');
     const data = await res.json();
@@ -29,16 +31,18 @@ export const getStaticProps = async (context) => {
 }
 
 const Details = ({ project }) => {
+    //console.log(project);
     return (           
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
                     <div className="section-title">
-                        <span className="caption d-block small">Categories</span>
-                        <h2>Projects</h2>
+                        <br />
+                        {/* <span className="caption d-block small">Categories</span> */}
+                        <h2>Project</h2>
                     </div>
                     <div className="post-entry-2 d-flex">    
-                        <img  width="100%" height="100%"  src={`https://aces-admin.herokuapp.com${project.ProjectImage}`} alt="Image" className="thumbnail order-md-2" /> 
+                        <img  width="100%" height="100%"  src={project.ProjectImage1.url} alt="Image" className="thumbnail order-md-2" /> 
                         <div className="contents order-md-1 pl-0">
                             <h2><a href="blog-single.html">{project.ProjectName}</a></h2>
                             <p className="mb-4">{project.Abstract}</p>
@@ -48,7 +52,7 @@ const Details = ({ project }) => {
                                     {project.supervisors.map((supervisor)=> (
                                         <div className="col-md-8">
                                             <div className="post-entry-1">                                         
-                                                <img  width="70" height="70" src={`https://aces-admin.herokuapp.com${supervisor.AuthorImage}`} alt="Image" className="img-fluid" /> 
+                                                <img  width="70" height="70" src={supervisor.SupervisorImage.url} alt="Image" className="img-fluid" /> 
                                                 <h2><a href="blog-single.html">{supervisor.SupervisorName}</a></h2>
                                             </div>
                                         </div>                
@@ -61,13 +65,32 @@ const Details = ({ project }) => {
                                     {project.authors.map((author)=> (
                                         <div className="col-md-8">
                                             <div className="post-entry-1">                                         
-                                                <img  width="70" height="70" src={`https://aces-admin.herokuapp.com${author.AuthorImage}`} alt="Image" className="img-fluid" /> 
+                                                <img  width="70" height="70" src={author.AuthorImage.url} alt="Image" className="img-fluid" /> 
                                                 <h2><a href="blog-single.html">{author.AuthorName}</a></h2>
                                             </div>
                                         </div>                
                                     ))}
                                 </div>
                             </div>
+
+                            <div className="col-lg-6">
+                                <div className="post-entry-2 d-flex">    
+                                   
+                                    <div className="col-md-8">
+                                        <div className="post-entry-1">                                         
+                                            <Link href={project.GithubRepoLink}><input type="submit" value="Github Repo" className="btn-2 btn-primary py-2 px-8" /></Link>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-8">
+                                        <div className="post-entry-1">
+                                            <Link href={project.ProjectPortfolioLink}><input type="submit" value="Portfolio" className="btn-2 btn-primary py-2 px-8" /></Link>
+                                        </div>
+                                    </div>                
+                                    
+                                </div>
+                            </div>
+
                         </div>
                     </div>   
                 </div>
