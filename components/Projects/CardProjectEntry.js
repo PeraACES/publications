85d3d687cf9bf51411b9487240c574babd8caa2e
Ganindu Dhananja <ganindudananja@gmail.com>
2021-07-20 01:58:38 +0530

    Create CardProjectEntry.js

---
+++ components/Projects/CardProjectEntry.js
@@ -0,0 +1,37 @@
+import moment from 'moment';
+import Link from 'next/link';
+import TextTruncate from 'react-text-truncate';
+
+export default function CardProjectEntry({ id, abstract, name, image, createdAt }) {
+  console.log(id, abstract, name, image.url, createdAt);
+  return (
+    <div className="post-entry-1" key={id}>
+      {!!image && <img src={image.url} alt={image.caption} class="img-fluid" />}
+      <div className="contents">
+        {!!name && (
+          <h2>
+            <Link href={`/projects/${id}`}>
+              <a>{name}</a>
+            </Link>
+          </h2>
+        )}
+        {!!abstract && (
+          <TextTruncate
+            line={4}
+            element="p"
+            truncateText="..."
+            text={abstract}
+            textTruncateChild={
+              <Link href={`/projects/${id}`}>
+                <a>View</a>
+              </Link>
+            }
+          />
+        )}
+        <div className="post-meta">
+          <span className="date-read">{moment(createdAt).fromNow()}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
