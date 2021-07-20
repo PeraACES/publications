ca02a1c6065aa16907124ec71668d36767e26a7f
Ganindu Dhananja <ganindudananja@gmail.com>
2021-07-20 09:19:49 +0530

    Meta tags and title moved

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import Layout from '../components/Layout/Layout';
-//import { Provider } from "react-redux";
-//import { store } from "../store";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>ESCaPe 2021 | Online Project Symposium UOP</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
