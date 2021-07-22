eab0ad1b507591cb33a862afd663b9035dd00fa3
Ganindu Dhananja <ganindudananja@gmail.com>
2021-07-23 01:22:02 +0530

    Added Single Project updates

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -3,9 +3,11 @@ import Link from 'next/link';
 import ErrorPage from 'next/error';
 import { getAllProjectSlugs, getProjectBySlug } from '../../lib/api';
 import Layout from '../../components/Layout/Layout';
+import SingleProjectEntry from '../../components/Projects/SingleProjectEntry';
+import ReactMarkdown from 'react-markdown';
 
 const Details = ({ project }) => {
-  // console.log(project);
+  console.log(project);
   const router = useRouter();
 
   if (!router.isFallback && !project?.slug) {
@@ -35,6 +37,17 @@ const Details = ({ project }) => {
       <div className="site-section" style={{ paddingBottom: '1rem' }}>
         <div className="container">
           <div className="row">
+            {/* <div className="col-lg-12">
+              <SingleProjectEntry
+                id={project.id}
+                name={project.ProjectName}
+                image={project.ProjectImage1}
+                slug={project.slug}
+                authors={project.authors}
+                supervisors={project.supervisors}
+                createdAt={project.createdAt}
+              />
+            </div> */}
             <div className="col-lg-12">
               <div className="section-title">
                 {/* <span className="caption d-block small">Categories</span> */}
@@ -49,7 +62,6 @@ const Details = ({ project }) => {
                   className="thumbnail order-md-1 mr-5"
                 />
                 <div className="contents order-md-2 pl-0 pt-0">
-                  <p className="mb-4">{project.Abstract}</p>
                   <h2>Supervisors</h2>
                   <div className="col-lg-6">
                     <div className="post-entry-2 d-flex">
@@ -92,39 +104,32 @@ const Details = ({ project }) => {
                       ))}
                     </div>
                   </div>
-
-                  <div className="col-lg-6">
-                    <div className="post-entry-2 d-flex">
-                      <div className="col-md-8">
-                        <div className="post-entry-1">
-                          {!!project.GithubRepoLink && (
-                            <Link href={project.GithubRepoLink}>
-                              <input
-                                type="submit"
-                                value="Github Repo"
-                                className="btn-2 btn-primary py-2 px-8"
-                              />
-                            </Link>
-                          )}
-                        </div>
-                      </div>
-
-                      <div className="col-md-8">
-                        <div className="post-entry-1">
-                          {!!project.ProjectPortfolioLink && (
-                            <Link href={project.ProjectPortfolioLink}>
-                              <input
-                                type="submit"
-                                value="Portfolio"
-                                className="btn-2 btn-primary py-2 px-8"
-                              />
-                            </Link>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg">
+                {!!project.Abstract && <ReactMarkdown>{project.Abstract}</ReactMarkdown>}
+              </div>
+              <div></div>
+            </div>
+            <div className="row">
+              <div className="col mr-5">
+                <p>
+                  <a href={project.ProjectPortfolioLink} class="more">
+                    More Information<span class="icon-keyboard_arrow_right"></span>
+                  </a>
+                </p>
+              </div>
+            </div>
+            <br />
+            <div className="row">
+              <div className="col">
+                <p>
+                  <a href={project.GithubRepoLink} class="more">
+                    GitHub Repository <span class="icon-keyboard_arrow_right"></span>
+                  </a>
+                </p>
               </div>
             </div>
           </div>
