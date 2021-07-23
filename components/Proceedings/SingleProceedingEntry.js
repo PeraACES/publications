0f93e94e8ff95e1252c26ec224485987c59f3adf
Dulanjali <dulapreethi@gmail.com>
2021-07-23 11:31:27 +0530

    precedings slug and designs added

---
+++ components/Proceedings/SingleProceedingEntry.js
@@ -0,0 +1,99 @@
+import Link from 'next/link';
+
+const Person = ({ id, title, image, link }) => (
+  <>
+    {!!id && (
+      <div className="col-md-8" key={id}>
+        <div className="post-entry-1">
+          {!!image && (
+            <img
+              width="70"
+              height="70"
+              className="img-fluid"
+              src={image.url}
+              alt={`person-${id}-image`}
+            />
+          )}
+          {!!link ? (
+            <a className="text-black mt-2" href={link} target="_blank" rel="noreferrer">
+              {name}
+            </a>
+          ) : (
+            <h6 className="text-black mt-2">{title}</h6>
+          )}
+        </div>
+      </div>
+    )}
+  </>
+);
+
+export default function SingleProceedingEntry({
+  id,
+  title,
+  image,
+  slug,
+  symposiumeditors,
+  symposiumchair,
+  createdAt
+}) {
+  return (
+    <>
+      <div className="section-title">{!!title && <h2>{title}</h2>}</div>
+      <div className="half-post-entry d-block d-lg-flex">
+        {/* {!!image && <img                   width="100%"
+                  height="100%" src={image.url} alt={image.caption} className="thumbnail mr-5" />} */}
+        <div className="contents">
+          <div className="row">
+            <h5>Symposium Chair</h5>
+            <div className="col-lg-6">
+              <div className="post-entry-2 d-flex">
+                <Person
+                  key={symposiumchair.SupervisorID}
+                  id={symposiumchair.SupervisorID}
+                  name={symposiumchair.SupervisorName}
+                  image={symposiumchair.SupervisorImage}
+                  link={symposiumchair.link}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <h5>Symposium Editors</h5>
+            <div className="container pt-3">
+              <div className="col-lg-6">
+                <div className="post-entry-2 d-flex">
+                  {!!symposiumeditors &&
+                    Array.isArray(symposiumeditors) &&
+                    symposiumeditors.map((item) => (
+                      <Person
+                        key={item.SupervisorID}
+                        id={item.SupervisorID}
+                        name={item.SupervisorName}
+                        image={item.SupervisorImage}
+                        link={item.link}
+                      />
+                    ))}
+                </div>
+              </div>
+            </div>
+            {/* <div className="container pt-3">
+              <div className="row justify-content-between">
+                {!!authors &&
+                  Array.isArray(authors) &&
+                  authors.map((item) => (
+                    <Person
+                      key={item.AuthorID}
+                      id={item.AuthorID}
+                      name={item.AuthorName}
+                      image={item.AuthorImage}
+                      link={item.link}
+                    />
+                  ))}
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/precedings.js
+++ pages/precedings.js
@@ -1,103 +1,118 @@
+// import { useEffect, useState } from 'react';
 import fetch from 'isomorphic-fetch';
-import Link from 'next/link';
 import Layout from '../components/Layout/Layout';
+// import { useRouter, withRouter } from 'next/router';
+// import Link from 'next/link';
+import ListProceedingEntry from '../components/Proceedings/ListProceedingEntry';
 import { API_URL } from '../config';
+import { getAllProceedings } from '../lib/api';
 
 // pagination url with filters
-// /proceedings?_limit=5&_start=0 // for first 5 items
-// /proceedings?_limit=10&_start=0 // for first 10 items
+// /projects?_limit=5&_start=0 // for first 5 items
+// /projects?_limit=10&_start=0 // for first 10 items
 
-const Precedings = ({ proceedings, error }) => {
-  //console.log(proceedings)
+// const pageSize = 5;
+
+export default function Proceedings({ data, error, href }) {
+  const { proceedings } = data;
+  //console.log(proceedings);
+
+  // const [page, setPage] = useState(null);
+
+  // const router = useRouter();
+  // console.log(href, router, router.query);
+  // // const { projects, count } = data;
+
+  // useEffect(() => {
+  //   const { query } = router;
+  //   console.log('router hook', query.page, !router.query.page && page === null);
+  //   if (!router.query.page && page === null) {
+  //     console.log('query.page not found');
+  //     router.query['page'] = 1;
+  //     router.push(router.pathname + '?page=1');
+  //   } else if (page === null) {
+  //     console.log('setPage', query);
+  //     setPage(query.page);
+  //   }
+  // }, [router]);
+
+  // useEffect(() => {
+  //   console.log('page', page);
+  // }, [page]);
+
   if (error) {
-    return <div>An error occured: {error.message}</div>;
-  }
-
-  return (
-    <Layout>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="section-title">
-              {/* <span className="caption d-block small">Categories</span>
-                        <h2>Projects</h2> */}
-            </div>
-            {proceedings.map((proceedings) => (
-              <div key={proceedings.id} className="post-entry-2 d-flex">
-                <img
-                  width="100%"
-                  height="100%"
-                  src={`${proceedings.image.url}`}
-                  alt="Image"
-                  className="thumbnail order-md-2"
-                />
-                <div className="contents order-md-1 pl-0">
-                  <h2>
-                    <a href="blog-single.html">{proceedings.title}</a>
-                  </h2>
-                  <h3 className="mb-3">{proceedings.Theme}</h3>
-                  <p className="mb-3">{proceedings.markup}</p>
-                  <div className="row">
-                    <div className="col-12">
-                      <Link href={'/proceedings/' + proceedings.id}>
-                        <input
-                          type="submit"
-                          value="View More"
-                          className="btn-2 btn-primary py-2 px-8"
-                        />
-                      </Link>
-                    </div>
-                  </div>
+    return (
+      <>
+        <div className="site-section" style={{ paddingBottom: '1rem' }}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <div className="section-title">
+                  <h2>An error occured!</h2>
                 </div>
               </div>
-            ))}
-            {/* <div className="row">
-                        <div className="col-lg-6">
-                            <ul className="custom-pagination list-unstyled">
-                            <li><a href="#">1</a></li>
-                            <li className="active">2</li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">4</a></li>
-                            </ul>
-                        </div>
-                    </div> */}
+            </div>
+          </div>
+        </div>
+        {/* <div>An error occured: {error.message}</div> */}
+      </>
+    );
+  }
+  
+  return (
+    <Layout>
+      <div className="site-section" style={{ paddingBottom: '1rem' }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="section-title">
+                <h2>All Proceedings</h2>
+              </div>
+              {!!proceedings &&
+                Array.isArray(proceedings) &&
+                proceedings.map((item) => (
+                  <ListProceedingEntry
+                    key={item.id}
+                    id={item.id}
+                    slug={item.slug}
+                    markup={item.markup}
+                    title={item.title}
+                    image={item.image}
+                    createdAt={item.createdAt}
+                    displayButton={true}
+                  />
+                ))}
+              {/* <div className="row">
+                <div className="col-lg-6">
+                  <ul className="custom-pagination list-unstyled">
+                    <li>
+                      <a href="#">1</a>
+                    </li>
+                    <li className="active">2</li>
+                    <li>
+                      <a href="#">3</a>
+                    </li>
+                    <li>
+                      <a href="#">4</a>
+                    </li>
+                  </ul>
+                </div>
+              </div> */}
+            </div>
           </div>
         </div>
       </div>
     </Layout>
   );
-};
+}
 
-Precedings.getInitialProps = async (ctx) => {
+export async function getStaticProps(context) {
   try {
-    // Parses the JSON returned by a network request
-    const parseJSON = (resp) => (resp.json ? resp.json() : resp);
-    // Checks if a network request came back fine, and throws an error if not
-    const checkStatus = (resp) => {
-      if (resp.status >= 200 && resp.status < 300) {
-        return resp;
-      }
-
-      return parseJSON(resp).then((resp) => {
-        throw resp;
-      });
+    const proceedings = await getAllProceedings();
+    return {
+      props: { data: { proceedings } }
     };
-
-    const headers = {
-      'Content-Type': 'application/json'
-    };
-
-    const proceedings = await fetch(API_URL + '/proceedings', {
-      method: 'GET',
-      headers
-    })
-      .then(checkStatus)
-      .then(parseJSON);
-
-    return { proceedings };
   } catch (error) {
     return { error };
   }
-};
-
-export default Precedings;
+}
