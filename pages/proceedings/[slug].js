4e6c8e3c61e0925f2a33126a3689c44123dc3a80
Dulanjali <dulapreethi@gmail.com>
2021-07-23 15:54:10 +0530

    proceeding pdf view

--- pages/proceedings/[slug].js
+++ pages/proceedings/[slug].js
@@ -5,8 +5,33 @@ import { getAllProceedingSlugs, getProceedingBySlug } from '../../lib/api';
 import Layout from '../../components/Layout/Layout';
 import SingleProceedingEntry from '../../components/Proceedings/SingleProceedingEntry';
 import ReactMarkdown from 'react-markdown';
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 
 const Details = ({ proceeding }) => {
+
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
   // console.log(project);
   const router = useRouter();
 
@@ -114,6 +139,31 @@ const Details = ({ proceeding }) => {
                 )}
               </div>
               <div></div>
+            </div>
+            <div className="row">
+              <div className="col-lg">
+                <Document
+                  file= {proceeding.ProceedingPDFUrl}
+                  onLoadSuccess={onDocumentLoadSuccess}
+                >
+                  <Page pageNumber={pageNumber} />
+                </Document>
+                <div>
+                  <p>
+                    Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+                  </p>
+                  <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+                    Previous
+                  </button>
+                  <button
+                    type="button"
+                    disabled={pageNumber >= numPages}
+                    onClick={nextPage}
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
