72a94e50576be79ebce83d65624bdf7b78ee68dd
Ganindu Dhananja <ganindudananja@gmail.com>
2021-07-31 16:23:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-const isProd = process.env.NODE_ENV === 'production';
+const webpack = require('webpack');
+
+const isProd = (process.env.NODE_ENV || 'production') === 'production';
+
+// Use the CDN in production and localhost for development.
+const assetPrefix = isProd ? 'https://aces.ce.pdn.ac.lk/publications' : '';
+
 
 module.exports = {
-    // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? 'https://aces.ce.pdn.ac.lk/publications' : '',
-}
+    exportPathMap: () => ({
+      '/': { page: '/' }
+    }),
+    assetPrefix,
+    webpack: (config) => {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix)
+        })
+      );
+      return config;
+    }
+};
