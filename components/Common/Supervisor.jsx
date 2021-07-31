cb042fef5ecc036f06fda41b9f7bae87f977c414
Ganindu Dhananja <ganindudananja@gmail.com>
2021-08-01 03:23:44 +0530

    Update Supervisor.jsx

--- components/Common/Supervisor.jsx
+++ components/Common/Supervisor.jsx
@@ -1,35 +1,33 @@
+const assetPrefix = process.env.ASSET_PREFIX;
+
 const Supervisor = ({ id, image, link, alt, name }) => (
   <div className="col m-2" key={id}>
     <a href={link} target="_blank" rel="noreferrer">
-      {!!image && image.url && (
-        <>
-          {!!image.url ? (
-            <img
-              width="70"
-              src={image.url}
-              alt={alt}
-              style={{ borderRadius: '3px', background: 'transparent' }}
-            />
-          ) : (
-            <img
-              width="70"
-              src={`${assetPrefix}/images/user-avatar.png`}
-              alt={alt}
-              style={{ borderRadius: '3px', background: 'transparent' }}
-            />
-          )}
-          <h2>
-            {!!link ? (
-              <a href={link} target="_blank" rel="noreferrer">
-                {name}
-              </a>
-            ) : (
-              <p>{name}</p>
-            )}
-          </h2>
-        </>
+      {!!!!image && image.url ? (
+        <img
+          width="70"
+          src={image.url}
+          alt={alt}
+          style={{ borderRadius: '3px', background: 'transparent' }}
+        />
+      ) : (
+        <img
+          width="70"
+          src={`${assetPrefix}/images/user-avatar.png`}
+          alt={alt}
+          style={{ borderRadius: '3px', background: 'transparent' }}
+        />
       )}
     </a>
+    {!!link ? (
+      <h2>
+        <a href={link} target="_blank" rel="noreferrer">
+          {name}
+        </a>
+      </h2>
+    ) : (
+      <h2>{name}</h2>
+    )}
   </div>
 );
 
