228f7835e123217f656c9c7e3c02e3fea4a4226f
Ganindu Dhananja <ganindudananja@gmail.com>
2021-07-31 16:51:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,16 +7,16 @@ const assetPrefix = isProd ? 'https://aces.ce.pdn.ac.lk/publications' : '';
 
 
 module.exports = {
-    exportPathMap: () => ({
-      '/': { page: '/' }
-    }),
-    assetPrefix,
-    webpack: (config) => {
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix)
-        })
-      );
-      return config;
-    }
+  exportPathMap: () => ({
+    '/': { page: '/' }
+  }),
+  assetPrefix,
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix)
+      })
+    );
+    return config;
+  }
 };
