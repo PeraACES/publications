8f9986abb5ac32d9f3e18e7b42f84791f0ac0af8
Ganindu Dhananja <ganindudananja@gmail.com>
2021-08-07 01:09:19 +0530

    Update Footer.jsx

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="container">
           {/* <form action="#" className="row align-items-center">
             <div className="col-md-5 mr-auto">
-              <h2>Newsletter Subcribe</h2>
+              <h3>Newsletter Subcribe</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
                 aspernatur ut at quae omnis pariatur obcaecati possimus nisi ea iste!
@@ -28,8 +28,62 @@ export default function Footer() {
           </form> */}
         </div>
       </div>
-      <div className="footer">
+      <div className="footer bg-black text-white">
         <div className="container">
+          <div className="row m-4">
+            <div className="col-md-6 col-12">
+              <div className="ftco-footer-widget mb-4 text-center">
+                <h3 className="ftco-heading-2">Useful Links</h3>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      href="http://www.ce.pdn.ac.lk/"
+                      className="py-2 d-block"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      Dept. of Computer Engineering
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://eng.pdn.ac.lk/"
+                      className="py-2 d-block"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      Faculty of Engineering
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://pdn.ac.lk/"
+                      className="py-2 d-block"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      University of Peradeniya
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6 col-12">
+              <div className="ftco-footer-widget mb-4 text-center">
+                <h3 className="ftco-heading-2">Have Questions?</h3>
+                <div className="block-23 m-3">
+                  <ul className="list-unstyled text-center">
+                    <li>
+                      <a href="mailto://aces@eng.pdn.ac.lk">
+                        <span className="icon icon-envelope"></span>
+                        <span className="text-info">aces@eng.pdn.ac.lk</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="row">
             <div className="col-12">
               <div className="copyright">
